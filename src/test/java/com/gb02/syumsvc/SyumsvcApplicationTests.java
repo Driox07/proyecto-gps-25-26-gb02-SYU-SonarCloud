400958c5c1f2894f160d674d18fadab1fc48041f
Adrián <driox@outlook.es>
2025-10-22 19:19:01 +0000

    Inicializado proyecto de Spring Boot

---
+++ src/test/java/com/gb02/syumsvc/SyumsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb02.syumsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyumsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
