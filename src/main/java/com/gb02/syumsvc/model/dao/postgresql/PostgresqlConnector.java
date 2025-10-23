f641e3b7376adf25ac690730acccb71578775361
Adrián <driox@outlook.es>
2025-10-23 10:32:59 +0000

    Eliminado import innecesario de PostgresqlConnector

--- src/main/java/com/gb02/syumsvc/model/dao/postgresql/PostgresqlConnector.java
+++ src/main/java/com/gb02/syumsvc/model/dao/postgresql/PostgresqlConnector.java
@@ -8,7 +8,6 @@ import java.sql.Connection ;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import org.postgresql.Driver;
 
 public class PostgresqlConnector {
     
