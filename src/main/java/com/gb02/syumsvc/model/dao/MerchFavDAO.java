9d9e49f3e0e1a8d9e8cec892c803f3ec4ecf035d
Adrián <driox@outlook.es>
2025-10-23 14:58:55 +0000

    Creado DAO para PostgreSQL/MerchFav

--- src/main/java/com/gb02/syumsvc/model/dao/MerchFavDAO.java
+++ src/main/java/com/gb02/syumsvc/model/dao/MerchFavDAO.java
@@ -3,10 +3,10 @@ package com.gb02.syumsvc.model.dao;
 import com.gb02.syumsvc.model.dto.MerchFavDTO;
 
 public interface MerchFavDAO {
-    public MerchFavDTO[] obtainMerchFavByUser(int userId);
-    public MerchFavDTO[] obtainMerchFavByMerch(int merchId);
+    public MerchFavDTO[] obtainMerchFavByUser(int idUsuario);
+    public MerchFavDTO[] obtainMerchFavByMerch(int idMerch);
     public MerchFavDTO[] obtainMerchFav();
-    public MerchFavDTO obtainMerchFav(int idUsuario, int idMerch);
+    public MerchFavDTO obtainMerchFav(int idMerch, int idUsuario);
     public boolean insertMerchFav(MerchFavDTO merchFav);
-    public boolean deleteMerchFav(int idUsuario, int idMerch);
+    public boolean deleteMerchFav(int idMerch, int idUsuario);
 }
